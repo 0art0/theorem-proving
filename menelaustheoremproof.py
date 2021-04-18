11f01f261d5f38d9b0bcab4196ca6a7931bda6cf
ART <anand.rao.art@gmail.com>
2021-04-18 16:03:41 +0530

    Add files via upload

---
+++ menelaustheoremproof.py
@@ -0,0 +1,62 @@
+# This is a proof of Menelaus' theorem (https://en.wikipedia.org/wiki/Menelaus%27s_theorem) using the z3 solver
+
+# %%
+from z3 import *
+
+# %%
+# A, B, C are the vertices of the triangle. 
+# WLOG, it is assumed to have a base of unit length on the x-axis, with the third point above the x-axis.
+A = (x_a, y_a) = Reals('x_a y_a')
+B = (x_b, y_b) = Reals('x_b y_b')
+C = (x_c, y_c) = Reals('x_c y_c')
+
+# Each of the edges of the triangle can be parameterised by a single variable,
+# which is equal to the first vertex at 0 and equal to the second vertex at 1
+r, s, t = Reals('r s t')
+
+def cut(l, P, Q):
+    #with the line PQ parameterised as described above,
+    #this function returns the point obtained when the parameter is equal to `l`
+    return (P[0] + l*(Q[0] - P[0]), P[1] + l*(Q[1] - P[1]))
+
+# D, E, F are points on the edges AB, BC, CA respectively
+D, E, F = cut(r, A, B), cut(s, B, C), cut(t, C, A)
+
+def are_collinear(p_1, p_2, p_3):
+    #the condition for collinearity
+    #(y_2 - y_1)*(x_3 - x_1) == (y_3 - y_1)*(x_3 - x_1)
+    return ( (p_2[1] - p_1[1])*(p_3[0] - p_1[0]) == (p_3[1] - p_1[1])*(p_2[0] - p_1[0]) )
+
+def square(x):
+    return x**2
+
+def d(p, q):
+    #returns the square of the Euclidean distance between points p and q
+    return square(p[0] - q[0]) + square(p[1] - q[1])
+
+def in_bounds(l):
+    #checks whether the parameter is within the range (0, 1)
+    #i.e, whether the point corresponding to the parameter value `l` is contained within the corresponding edge or on an extension of it
+    return And(0 < l, l < 1)
+
+# for the converse of Menelaus theorem to hold, the number of intersection points lying on the extensions of edges must either be 1 or 3 (https://brilliant.org/wiki/menelaus-theorem/#theorem)
+# the following two expressions capture these conditions
+# alternatively, one can define "signed" distances to make the theorem more general
+
+one_not_in_bounds = Or([
+    And([Not(in_bounds(r)), in_bounds(s), in_bounds(t)]),
+    And([in_bounds(r), Not(in_bounds(s)), in_bounds(t)]),
+    And([in_bounds(r), in_bounds(s), Not(in_bounds(t))])
+])
+three_not_in_bounds = And([Not(in_bounds(r)), Not(in_bounds(s)), Not(in_bounds(t))])
+
+# this is the equation that the distances (rather, the squares of the distances) must satisfy
+dist_eq = d(A, D) * d(B, E) * d(C, F) == d(D, B) * d(E, C) * d(F, A)
+# %%s
+#these are the "forward" and "backward" statements of the theorem
+menelaus_thm_fwd = Implies(And(Not(are_collinear(A, B, C)), are_collinear(D, E, F)), dist_eq)
+menelaus_thm_bwd = Implies(And(Not(are_collinear(A, B, C)), Or(one_not_in_bounds, three_not_in_bounds), dist_eq), are_collinear(D, E, F))
+# %%
+#proving the theorems by contradiction
+solve(Not(menelaus_thm_fwd))
+solve(Not(menelaus_thm_bwd))
---
+++ pappustheoremz3solver.py
@@ -0,0 +1,60 @@
+# %%
+from z3 import *
+
+def point(i):
+    exec("x{j} = Real('x{j}')".format(j = i))
+    exec("y{j} = Real('y{j}')".format(j = i))
+
+    return eval("(x{j}, y{j})".format(j = i))
+
+# %%
+#initialising all the points
+
+# `a b c` are collinear
+# `A B C` are collinear
+
+# `P Q R` are the points of intersection of the pairs of lines (Ab, Ba), (Bc, Cb), (Ca, Ac) respectively
+# In other words, P is a point that lies on the lines Ab and Ba, and likewise with the other two points
+
+for (i, s) in enumerate(('a', 'b', 'c', 'A', 'B', 'C', 'P', 'Q', 'R')):
+    #initialise the variables
+    exec("x_{j} = Real('x_{j}')".format(j = i))
+    exec("y_{j} = Real('y_{j}')".format(j = i))
+
+    #create the point
+    exec("{v} = (x_{j}, y_{j})".format(v = s, j = i))
+
+# %%
+def are_collinear(p_1, p_2, p_3):
+    #the condition for collinearity
+    #(y_2 - y_1)*(x_3 - x_1) == (y_3 - y_1)*(x_3 - x_1)
+    return ( (p_2[1] - p_1[1])*(p_3[0] - p_1[0]) == (p_3[1] - p_1[1])*(p_2[0] - p_1[0]) )
+
+def all_distinct(pts):
+    #checks whether all points in the list `pts` are distinct
+    return [Or(Not(p[0] == q[0]), Not(p[1] == q[1])) for (i, p) in enumerate(pts) for (j, q) in enumerate(pts) if p != q]
+
+def parallel(p_1, p_2, q_1, q_2):
+    #checks whether the line passing through p_1 and p_2 is parallel to 
+    # the line passing through q_1 and q_2
+
+    return (p_2[1] - p_1[1])*(q_2[0] - q_1[0]) == (p_2[0] - p_1[0])*(q_2[1] - q_1[1])
+
+# %%
+#this is a formulation of the second statement here - https://en.wikipedia.org/wiki/Pappus's_hexagon_theorem#Other_statements_of_the_theorem
+
+
+#there were issues when the lines were parallel, hence the extra condition `Not(parallel(A, B, a, b))`
+#to prove the theorem in full generality, one can independently prove the case where the two lines are parallel (this should be simpler than the general case)
+
+pappus_theorem = Implies(And([are_collinear(p, q, r) for (p, q, r) in (
+    (a, b, c), (A, B, C),
+    (A, b, P), (B, a, P),
+    (B, c, Q), (C, b, Q),
+    (C, a, R), (A, c, R)
+)] + [Not(parallel(A, B, a, b))]
+), are_collinear(P, Q, R))
+# %%
+soln = solve(Not(pappus_theorem))
+
+# %%
